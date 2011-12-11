15fb4313118190151ea96326cba4facb654460f9
Clement Escoffier <clement.escoffier@gmail.com>
2011-12-11 19:10:12 +0100

    Fixed https://github.com/akquinet/chameRIA/issues/3 : example distribution not able to run out of the box
    Set the path correctly on Windows.

--- chameria-launcher/src/main/resources/launch-debug-win.bat
+++ chameria-launcher/src/main/resources/launch-debug-win.bat
@@ -1,5 +1,4 @@
-SET PATH=.\lib;%PATH%
-
+SET PATH=.\lib;.\qt\bin;%PATH%
 SET CLASSPATH=core/chameria-launcher-${chameria.version}.jar;core/core-${chameleon.version}.jar;core/logback-classic-${logback.version}.jar;core/org.osgi.compendium-4.2.0.jar;core/logback-core-${logback.version}.jar;core/org.apache.felix.framework-${felix.version}.jar;core/slf4j-api-${slf4j.version}.jar;qt/qtjambi-${qt.version}-jnlp.jar;qt/qtjambi-util-${qt.version}.jar
 
 java -Dchameleon.log.level=INFO -Dcom.trolltech.qt.library-path-override=qt/bin de.akquinet.chameria.launcher.Launcher  --config=conf/chameleon.properties --debug --deploy=deploy %*
